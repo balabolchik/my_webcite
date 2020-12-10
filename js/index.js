eda1cf4d1ab2d1f599cfbcfba1a48a16e772353c
balabolchik <obordulyak@gmail.com>
2020-12-10 21:59:57 +0200

    registration

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let newUserName = document.getElementById('first-name');
+let newUserSurname = document.getElementById('surname');
+let buttonForSaveInformation = document.getElementById('save-botton');
+let id = 0;
+let users = {};
+buttonForSaveInformation.addEventListener('click', () => {
+    id++;
+    users[id] = {};
+    users[id].firstName = newUserName.value;
+    users[id].surname = newUserSurname.value;
+    alert(users[id]);
+});
